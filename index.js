6bdc0556092a84b632c492303901f7d041d9bfb2
sahaarnav3 <sahaarnav3@gmail.com>
2025-06-29 04:14:10 +0530

    Added route to delete a job with Job-Id.

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ app.get("/", (req, res) => {
   res.send("<h1>Hi, Welcome to homepage of this Backend App.</h1>");
 });
 
+//Route to add a new JOB.
 app.post("/job", async (req, res) => {
   try {
     const job = new Job(req.body);
@@ -46,17 +47,42 @@ app.post("/job", async (req, res) => {
 // use -- "/job?jobId=..." to fetch a particular job or dont use query params to fetch all jobs;
 app.get("/job", async (req, res) => {
   try {
-    if(req.query.jobId){
-        const jobResponse = await Job.findById(req.query.jobId).exec();
-        if(!jobResponse)
-            return res.status(404).json({error: "Jobs with given JobId can't be found. Try again with correct ID."});
-        return res.status(200).json(jobResponse);
+    if (req.query.jobId) {
+      const jobResponse = await Job.findById(req.query.jobId).exec();
+      if (!jobResponse)
+        return res
+          .status(404)
+          .json({
+            error:
+              "Jobs with given JobId can't be found. Try again with correct ID.",
+          });
+      return res.status(200).json(jobResponse);
     } else {
-        const jobResponse = await Job.find();
-        if(!jobResponse)
-            return res.status(404).json({error: "Some error occurred with the request. Please try again."});
-        return res.status(200).json(jobResponse);
+      const jobResponse = await Job.find();
+      if (!jobResponse)
+        return res
+          .status(404)
+          .json({
+            error: "Some error occurred with the request. Please try again.",
+          });
+      return res.status(200).json(jobResponse);
     }
+  } catch (error) {
+    console.log(err);
+    res.status(500).json({
+      error:
+        "Some error occurred with the request itself. Please check logs and try again.",
+    });
+  }
+});
+
+//Route to delete a Job with Job-ID.
+app.delete("/job/:jobId", async (req, res) => {
+  try {
+    const jobResponse = await Job.findByIdAndDelete(req.params.jobId);
+    if(!jobResponse)
+        return res.status(400).json({error: "Either the given Job-Id doesn't exist or there is some other issue. Please try again with correct Job-ID."});
+    return res.status(200).json({message: "Success. Job Deleted."});
   } catch (error) {
     console.log(err);
     res.status(500).json({
